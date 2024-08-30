9666605426f8d566d4fa46f36a407bb8aab9fab2
Georgie Tasev <georgi.tasev12@gmail.com>
2024-08-30 18:14:21 +0300

    Update on home_page.dart and added Expanded widget on ListView and it's items.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -49,72 +49,74 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             )
-          : Padding(
-              padding: const EdgeInsets.all(Constants.kOverallPadding),
-              child: ListView.separated(
-                itemCount: articleProvider.articlesList.articles.length,
-                separatorBuilder: (context, index) => const SizedBox(height: 8),
-                itemBuilder: (context, index) {
-                  final article = articleProvider.articlesList.articles[index];
+          : Expanded(
+            child: Padding(
+                padding: const EdgeInsets.all(Constants.kOverallPadding),
+                child: ListView.separated(
+                  itemCount: articleProvider.articlesList.articles.length,
+                  separatorBuilder: (context, index) => const SizedBox(height: 8),
+                  itemBuilder: (context, index) {
+                    final article = articleProvider.articlesList.articles[index];
 
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        article.title,
-                        style: const TextStyle(
-                          color: Colors.black87,
-                          fontWeight: FontWeight.bold,
-                          fontSize: Constants.kTitleFontSize,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: Constants.kLeftPaddingPublishedAt,
-                          top: Constants.kTopPaddingPublishedAt,
-                        ),
-                        child: Text(
-                          article.publishedAt.toString().substring(
-                                0,
-                                article.publishedAt.toString().length - 14,
-                              ),
-                          style: const TextStyle(color: Colors.lightBlueAccent),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Image.network(article.urlToImage),
-                      const SizedBox(height: 30),
-                      Text(
-                        article.description,
-                        style: const TextStyle(
-                          color: Color(Constants.darkishGrey),
-                          fontSize: Constants.kDescriptionFontSize,
-                        ),
-                      ),
-                      SourceTextLink(
-                        onTap: () => launchUrl(Uri.parse(article.url)),
-                        text: 'Source: ${article.url}',
-                        isHovered: articleProvider.isHovered,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                top: Constants.kTopPaddingSourceName),
-                            child: Text(
-                              "Article written by: ${article.source.name}",
-                              style: const TextStyle(color: Colors.grey),
-                            ),
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          article.title,
+                          style: const TextStyle(
+                            color: Colors.black87,
+                            fontWeight: FontWeight.bold,
+                            fontSize: Constants.kTitleFontSize,
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 40),
-                    ],
-                  );
-                },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: Constants.kLeftPaddingPublishedAt,
+                            top: Constants.kTopPaddingPublishedAt,
+                          ),
+                          child: Text(
+                            article.publishedAt.toString().substring(
+                                  0,
+                                  article.publishedAt.toString().length - 14,
+                                ),
+                            style: const TextStyle(color: Colors.lightBlueAccent),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Image.network(article.urlToImage),
+                        const SizedBox(height: 30),
+                        Text(
+                          article.description,
+                          style: const TextStyle(
+                            color: Color(Constants.darkishGrey),
+                            fontSize: Constants.kDescriptionFontSize,
+                          ),
+                        ),
+                        SourceTextLink(
+                          onTap: () => launchUrl(Uri.parse(article.url)),
+                          text: 'Source: ${article.url}',
+                          isHovered: articleProvider.isHovered,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: Constants.kTopPaddingSourceName),
+                              child: Text(
+                                "Article written by: ${article.source.name}",
+                                style: const TextStyle(color: Colors.grey),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 40),
+                      ],
+                    );
+                  },
+                ),
               ),
-            ),
+          ),
     );
   }
 }
