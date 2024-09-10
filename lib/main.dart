b7cdbd5d078fab4ff7807e04cc6733a70e826d4b
Georgie Tasev <georgi.tasev12@gmail.com>
2024-09-10 15:29:59 +0300

    Updated the SystemChrome to use only portrait mode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:news_api/http/http.dart';
 import 'package:news_api/providers/home_page_provider.dart';
 import 'package:news_api/repositories/repository_articles.dart';
@@ -20,6 +21,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
