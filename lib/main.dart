d0c32321046b1e424e6f10c83621e0fc6ff8623b
Georgie Tasev <georgi.tasev12@gmail.com>
2024-09-10 15:18:39 +0300

    Added the capability to use only Portrait Mode on the list.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:news_api/http/http.dart';
 import 'package:news_api/providers/home_page_provider.dart';
 import 'package:news_api/repositories/repository_articles.dart';
@@ -6,13 +7,10 @@ import 'package:provider/provider.dart';
 import 'ui/home_page.dart';
 
 void main() {
-  runApp(
-      ChangeNotifierProvider(
-          create: (BuildContext context) =>
-              ArticleProvider(ArticlesRepository(ApiService())),
-          child: const MyApp()
-      )
-  );
+  runApp(ChangeNotifierProvider(
+      create: (BuildContext context) =>
+          ArticleProvider(ArticlesRepository(ApiService())),
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,6 +18,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
