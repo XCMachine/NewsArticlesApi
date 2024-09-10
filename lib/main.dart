738108b9fc0b23ea1f52c0cdea7e1a4f5b7735da
Georgie Tasev <georgi.tasev12@gmail.com>
2024-09-10 15:27:33 +0300

    Added SystemChrome to set to portrait only.

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
